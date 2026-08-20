5363a972b140366e3670811618b02e3e1da9c79c
Akshaya aman <akshayaaman01@gmail.com>
2026-08-20 23:03:15 +0530

    Time: 15 ms (35.85%) | Memory: 11.7 MB (26.83%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,56 +1,56 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     string minWindow(string s, string t) {
-        vector<int> need(128, 0);
-        vector<int> window(128, 0);
-        for (char ch : t) {
-            need[ch]++;
+
+        if (s.empty() || t.empty())
+            return "";
+
+        unordered_map<char, int> need;
+        unordered_map<char, int> window;
+
+     
+        for (char c : t) {
+            need[c]++;
         }
 
-        int required = 0;
-        for (int i = 0; i < 128; i++) {
-            if (need[i] > 0) {
-                required++;
-            }
-        }
-
-        int formed = 0;
         int left = 0;
-
-        int minLength = INT_MAX;
+        int count = 0;
+        int minLen = INT_MAX;
         int start = 0;
 
-        for (int right = 0; right < s.length(); right++) {
-            char ch = s[right];
+        for (int right = 0; right < s.size(); right++) {
 
-            window[ch]++;
-            if (need[ch] > 0 && window[ch] == need[ch]) {
-                formed++;
+        
+            window[s[right]]++;
+
+    
+            if (need.count(s[right]) &&
+                window[s[right]] <= need[s[right]]) {
+                count++;
             }
-            while (formed == required) {
-                if (right - left + 1 < minLength) {
-                    minLength = right - left + 1;
+
+        
+            while (count == t.size()) {
+
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
                     start = left;
                 }
 
-                char leftChar = s[left];
-                window[leftChar]--;
-
-                if (need[leftChar] > 0 &&
-                    window[leftChar] < need[leftChar]) {
-                    formed--;
+           
+                if (need.count(s[left]) &&
+                    window[s[left]] <= need[s[left]]) {
+                    count--;
                 }
 
+                window[s[left]]--;
                 left++;
             }
         }
-        if (minLength == INT_MAX) {
-            return "";
-        }
 
-        return s.substr(start, minLength);
+        if (minLen == INT_MAX)
+            return "";
+
+        return s.substr(start, minLen);
     }
 };
